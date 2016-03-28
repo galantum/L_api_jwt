9941d7a94007a64981791ff45d38bb6153a7fda8
galantum <grandisGT@gmail.com>
2016-03-28 12:17:23 +0700

    Create function to handle request
    Membuat algoritma untuk menghandel semua request yang masuk

--- app/Http/Middleware/jwtMiddleware.php
+++ app/Http/Middleware/jwtMiddleware.php
@@ -15,6 +15,40 @@ class jwtMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        #create variable to get token from Authorization field
+        $jwt = $request->header('Authorization');
+        
+        #create conditional statment if Token JWT null
+        if (is_null($jwt)) {
+            return response([ 'error' => 'Unauthorized.'], 401);
+        } else {
+            #Initialitated token signature
+            $key = 'grandis';
+
+            try{
+                #decode Token JWT
+                $decoded = JWT::decode($jwt, $key, array('HS256'));
+                #Change decode Token JWT to array
+                $decoded_array = (array)$decoded;
+
+                #get value email and password from decoded_array
+                $email = $decoded_array['email'];
+                $password = $decoded_array['password'];
+
+                #input value email and password in variable $data
+                $data = ['email' => $email, 'password' => $password];
+
+                #create conditional statment if Auth match with value $data
+                if (Auth::attempt($data)) {
+                    #Authentication success
+                    return $next($request);
+                } else {
+                    #Authentication failed
+                    return response([ 'error' => 'Unauthorized.'], 401);
+                }
+
+            }catch(\Exception $e){
+                return response(['error' => $e->getMessage()], 500);
+            }
     }
 }
