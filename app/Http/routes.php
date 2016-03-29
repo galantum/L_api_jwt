9cd9902a8583571ab0a84d237d5610b13456e2aa
galantum <grandisGT@gmail.com>
2016-03-29 18:52:27 +0700

    Change route group with create nested group
    Merubah Route group dengan membuat nested group. Ini dilakukan agar
    nilai Auth yang didapat setelah login, dapat digunakan di setiap
    function yang ada pada class UserController.
    Agar tidak error saat login dengan client-side maka code untuk
    inisialisasi class VerifyCsrfToken harus di hapus dari class kernel.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,25 +11,28 @@
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
+/*Route::group(['middleware' => ['web']], function () {
 
     Route::get('/', function () {
         return view('welcome');
     });
 
-});
+});*/
 
 #create route for registration
 Route::post('/register', 'UserController@register');
 
-#Create route for Login
-Route::post('/login', 'UserController@login');
+#Create nested group route
+Route::group(['middleware' => ['web']], function () {
+	#Create route for Login
+	Route::post('/login', 'UserController@login');
 
-#create route group for user access his data
-Route::group(array('prefix' => 'api', 'middleware' => 'jwt'), function(){
-	#Create route for get data profile User
-	Route::get('/user', 'UserController@show');
-	#Create Route for edit data profile User
-	Route::put('/user/edit', 'UserController@edit');
+	#create route group for user access his data
+	Route::group(array('prefix' => 'api', 'middleware' => 'jwt'), function(){
+		#Create route for get data profile User
+		Route::get('/user', 'UserController@show');
+		#Create Route for edit data profile User
+		Route::put('/user/edit', 'UserController@edit');
 
+	});
 });
